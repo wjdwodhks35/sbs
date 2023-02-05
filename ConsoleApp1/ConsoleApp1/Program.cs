452b35c5d88096d6cb6cf3af3daa15372950c9d3
treelun <109446729+treelun@users.noreply.github.com>
2023-02-05 17:49:55 +0900

    sbs
    둘쨋날

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            //1.이름 입력 받기
+            string Name;
+            Console.Write("이름 입력 : ");
+            Name = Console.ReadLine();
+            //2.생년 입력 받기
+            string BirthYear;
+            Console.Write("생년 입력 : ");
+            BirthYear = Console.ReadLine();
+            //3.자기소개 입력받기
+            string Introduce;
+            Console.Write("자기소개 입력 : ");
+            Introduce = Console.ReadLine();
+            //4.string으로 출력
+            int Age;
+            Console.WriteLine(Name);
+            Age = 2023 - Convert.ToInt32(BirthYear) + 1;
+            Console.WriteLine(Age);
+            Console.WriteLine(Introduce);
+            //5.$넣어서 표현
+            Console.WriteLine($"{Name} {Age} {Introduce}");
+            //6.@넣어서 사각형 만들기
+
+            Console.WriteLine(@" ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+ㅣ		      ㅣ
+ㅣ		      ㅣ
+ㅣ		      ㅣ
+ㅣ		      ㅣ
+ㅣ		      ㅣ
+ㅣ		      ㅣ
+ ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+            //7.1,2,3 string배열 넣기
+            string[] str1 = new string[3] { Name, BirthYear, Introduce };
+            //8. string 배열값을 4번과 같은 방식으로 출력하기
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(Introduce);
+            //9. string 배열값을 5번과 같은 방식으로 출력하기
+            Console.WriteLine($"{Name} {Age} {Introduce}");
+
+        }
+    }
+}
---
+++ ConsoleApp1/homework/Program.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//int number1 = 10;
+			//int number2 = 16;
+
+			//int plusResult = number1 + number2;
+			//int minuResult = number1 - number2;
+			//int multResult = number1 * number2;
+			//int divideResult = number1 / number2;
+			//int reminResult = number1 % number2;
+
+			//Console.WriteLine(plusResult);
+			//Console.WriteLine(minuResult);
+			//Console.WriteLine(multResult);
+			//Console.WriteLine(divideResult);
+			//Console.WriteLine(reminResult);
+
+			//int number11 = 123;
+			//int number21 = 532;
+
+			//number21 += number11;
+
+			//Console.WriteLine($"pp{number21}");
+			//number21 -= number11;
+			//Console.WriteLine($"pp{number21}");
+			//number21 *= number11;
+			//Console.WriteLine($"pp{number21}");
+			//number21 /= number11;
+			//Console.WriteLine($"pp{number21}");
+
+			//int vale = 123;
+
+			//Console.WriteLine($"전위형 {++vale}");
+			//Console.WriteLine($"후위형 {vale++}");
+
+			//number1 = 10;
+			//number2 = 16;
+
+			//plusResult = number1 + number2;
+			//minuResult = number1 - number2;
+			//multResult = number1 * number2;
+			//divideResult = number1 / number2;
+			//reminResult = number1 % number2;
+
+			//Console.WriteLine(plusResult);
+			//Console.WriteLine(minuResult);
+			//Console.WriteLine(multResult);
+			//Console.WriteLine(divideResult);
+			//Console.WriteLine(reminResult);
+
+			//number11 = 123;
+			//number21 = 532;
+
+			//number21 += number11;
+
+			//Console.WriteLine($"pp{number21}");
+			//number21 -= number11;
+			//Console.WriteLine($"pp{number21}");
+			//number21 *= number11;
+			//Console.WriteLine($"pp{number21}");
+			//number21 /= number11;
+			//Console.WriteLine($"pp{number21}");
+
+			//vale = 123;
+
+			//Console.WriteLine($"전위형 {++vale}");
+			//Console.WriteLine($"후위형 {vale++}");
+
+			//bool isTrue = true;
+			//bool isFalse = false;
+
+			//Console.WriteLine(isTrue);
+			//Console.WriteLine(isFalse);
+
+			//bool isSame = (0 == 0);
+			//Console.WriteLine(isSame);
+			//bool isDiffer = (0 == 1);
+			//Console.WriteLine(isDiffer);
+			//bool isBig = (0 > 1);
+			//Console.WriteLine(isBig);
+			//bool isSmall = (0 < 1);
+			//Console.WriteLine(isSmall);
+			//bool isBigorSame = (0 >= 1);
+			//Console.WriteLine(isBigorSame);
+			//bool isSmallorSame = (0 <= 1);
+			//Console.WriteLine(isSmallorSame);
+
+			//bool isAnd1 = (isTrue && isTrue);
+			//bool isAnd2 = (isTrue && isFalse);
+			//bool isAnd3 = (isFalse && isTrue);
+			//bool isAnd4 = (isFalse && isFalse);
+			//Console.WriteLine($"{isAnd1} {isAnd2} {isAnd3} {isAnd4}");
+
+			//bool isOr1 = (isTrue || isTrue);
+			//bool isOr2 = (isTrue || isFalse);
+			//bool isOr3 = (isFalse || isTrue);
+			//bool isOr4 = (isFalse || isFalse);
+			//Console.WriteLine($"{isOr1} {isOr2} {isOr3} {isOr4}");
+
+			//bool isTrue = true;
+			//bool isFalse = false;
+
+			//Console.WriteLine(isTrue);
+			//Console.WriteLine(isFalse);
+
+			//bool isSame = (0 == 0);
+			//Console.WriteLine(isSame);
+			//bool isDiffer = (0 == 1);
+			//Console.WriteLine(isDiffer);
+			//bool isBig = (0 > 1);
+			//Console.WriteLine(isBig);
+			//bool isSmall = (0 < 1);
+			//Console.WriteLine(isSmall);
+			//bool isBigorSame = (0 >= 1);
+			//Console.WriteLine(isBigorSame);
+			//bool isSmallorSame = (0 <= 1);
+			//Console.WriteLine(isSmallorSame);
+
+			//bool isAnd1 = (isTrue && isTrue);
+			//bool isAnd2 = (isTrue && isFalse);
+			//bool isAnd3 = (isFalse && isTrue);
+			//bool isAnd4 = (isFalse && isFalse);
+			//Console.WriteLine($"{isAnd1} {isAnd2} {isAnd3} {isAnd4}");
+
+			//bool isOr1 = (isTrue || isTrue);
+			//bool isOr2 = (isTrue || isFalse);
+			//bool isOr3 = (isFalse || isTrue);
+			//bool isOr4 = (isFalse || isFalse);
+			//Console.WriteLine($"{isOr1} {isOr2} {isOr3} {isOr4}");
+
+			//if      (false)
+			//{
+			//    Console.WriteLine("if문 실행 성공");
+			//}
+			//else if (false)
+			//{
+			//    Console.WriteLine("else if문 실행 성공");
+			//}
+			//else if (false)
+			//{
+			//    Console.WriteLine("else if문 실행 성공");
+			//}
+			//else
+			//{
+			//    Console.WriteLine("else문 실행");
+			//}
+			//Console.WriteLine("숫자 두개 입력");
+			//string str1=Console.ReadLine();
+			//string str2=Console.ReadLine();
+
+			//int number1 = Convert.ToInt32(str1);
+			//int number2 = Convert.ToInt32(str2);
+
+			//int leftNm = number1 % 2;
+			//if (leftNm == 0 && number2 < 0)
+			//{
+			//    Console.WriteLine("첫번째 값은 짝수이고, 두번째 값은 0보다 작습니다.");
+			//}
+			//else if (leftNm == 0 && number2 <3)
+			//{
+			//    Console.WriteLine("첫번째 값은 짝수이고, 두번째 값은 3보다 작습니다.");
+			//}
+			//else if (leftNm == 1 && number2 < 5)
+			//{
+			//    Console.WriteLine("첫번째 값은 홀수이고, 두번째 값은 5보다 작습니다.");
+			//}
+			//else
+			//{
+			//    Console.WriteLine("해당하지 않습니다.");
+			//}
+
+			//Console.Write("반복할 횟수 입력 : ");
+			//string str = Console.ReadLine();
+			//int numberLimit = Convert.ToInt32(str);
+			//while(numberLimit > 0)
+			//{
+			//    Console.WriteLine("Hi");
+
+			//    numberLimit--;
+			//}
+
+			//do
+			//{
+			//    Console.WriteLine("Hi");
+			//} while (numberLimit > 5);
+
+			//for (int number = 1; number < 6; number++)
+			//{
+			//    int n1 = 0;
+			//    while (n1 < number)
+			//    {
+			//        Console.Write("ㅇ");
+			//        n1++;
+			//    }
+			//    Console.WriteLine();
+			//}
+			//for (int number = 5; number > 0; number--)
+			//{
+			//    int n1 = 0;
+			//    while (n1 < number)
+			//    {
+			//        Console.Write("ㅇ");
+			//        n1++;
+			//    }
+			//    Console.WriteLine();
+			//}
+			//int n3 = 1;
+			//for (int number = 6; number > 1; number--)
+			//{
+			//    for (int n2 = 0; n2 < number; n2++)
+			//    {
+			//        Console.Write("  ");
+			//    }
+			//    for (int n1 = 0; n1 < n3; n1++)
+			//    {
+			//        Console.Write("ㅇ");
+			//    }
+			//    n3++;
+			//    Console.WriteLine();
+			//}
+			//int n4 = 5;
+			//int n5 = 0;
+			//for (int number = 0; number < 6; number++)
+			//{
+			//    for (int n2 = 0; n2 < number; n2++)
+			//    {
+			//        Console.Write("  ");
+			//    }
+			//    for (; n5 < n3; n5++)
+			//    {
+			//        Console.Write("ㅇ");
+			//    }
+			//    n4--;
+			//    Console.WriteLine();
+			//}
+
+
+			//int myNumber = 3;
+
+			//switch(myNumber)
+			//{
+			//    case 0:
+			//        Console.WriteLine("0");
+			//        break;
+			//    case 1:
+			//        Console.WriteLine("1");
+			//        break;
+			//    case 2:
+			//        Console.WriteLine("2");
+			//        break;
+			//    case 3:
+			//        Console.WriteLine("3");
+			//        break;
+			//    default:
+			//        break;
+			//for(int i = 0; i<5;i++)
+			//{
+			//    if (i == 0)
+			//    {
+			//        Console.WriteLine("0");
+			//        continue;
+			//    }
+			//    else if (i == 1)
+			//    {
+			//        Console.WriteLine("1");
+			//        Console.WriteLine("11");
+			//        continue;
+			//        Console.WriteLine("111");
+			//    }
+			//    else if (i == 2)
+			//    {
+			//        Console.WriteLine("2");
+			//        break;
+			//    }
+			//    else if (i == 3)
+			//    {
+			//        Console.WriteLine("3");
+			//    }
+			//}
+			//Console.WriteLine("끝");
+			//int newInt1 = Plus(5);
+			//int newInt2 = Minu(5);
+			//int newInt3 = Gop(5);
+			//int newInt4 = Nanum(5);
+			//int newInt5 = Mog(5);
+
+			//Console.WriteLine(newInt1);
+			//Console.WriteLine(newInt2);
+			//Console.WriteLine(newInt3);
+			//Console.WriteLine(newInt4);
+			//Console.WriteLine(newInt5);
+
+			//int n = Convert.ToInt32(Console.ReadLine());
+			//int jug = Jug(n);
+			//if (jug == 1) Console.WriteLine("홀수입니다.");
+			//if (jug == 0) Console.WriteLine("짝수입니다.");
+
+			//string s1 = Console.ReadLine();
+			//int s1Number = Convert.ToInt32(s1);
+
+			//string s3 = Console.ReadLine();
+
+			//string s2 = Console.ReadLine();
+			//int s2Number = Convert.ToInt32(s2);
+
+			//if (s3 == "+")
+			//{
+			//	int newInt = PlusTwoParam(s1Number, s2Number);
+			//	Console.WriteLine(newInt);
+			//}
+			//else if (s3 == "-")
+			//{
+			//	int newInt = MinuTwoParam(s1Number, s2Number);
+			//	Console.WriteLine(newInt);
+			//}
+			//else if (s3 == "*")
+			//{
+			//	int newInt = GopTwoParam(s1Number, s2Number);
+			//	Console.WriteLine(newInt);
+			//}
+			//else if (s3 == "/")
+			//{
+			//	int newInt = NanumTwoParam(s1Number, s2Number);
+			//	Console.WriteLine(newInt);
+			//}
+			//else Console.WriteLine("해당하지 않습니다.");
+		//	Console.Write("반복할 횟수 입력 : ");
+		//	int n  = Convert.ToInt32(Console.ReadLine());
+		//	Str(n);
+		//}
+        //private static int Plus(int number)
+        //{
+        //    int newNumber = number + 5;
+
+        //    return newNumber;
+        //}
+        //private static int Minu(int number)
+        //{
+        //    int newNumber = number - 5;
+
+        //    return newNumber;
+        //}
+        //private static int Gop(int number)
+        //{
+        //    int newNumber = number * 5;
+
+        //    return newNumber;
+        //}
+        //private static int Nanum(int number)
+        //{
+        //    int newNumber = number / 5;
+
+        //    return newNumber;
+        //}private static int Mog(int number)
+        //{
+        //    int newNumber = number % 5;
+
+        //    return newNumber;
+        //private static int Jug(int number)
+        //{
+        //	int jj = number % 2;
+        //	return jj;
+        //}
+        //private static int PlusTwoParam(int number1, int number2)
+        //      {
+        //	int resultNumber = number1 + number2;
+        //	return resultNumber;
+        //      }
+        //private static int MinuTwoParam(int number1, int number2)
+        //      {
+        //	int resultNumber = number1 - number2;
+        //	return resultNumber;
+        //      }
+        //private static int GopTwoParam(int number1, int number2)
+        //      {
+        //	int resultNumber = number1 * number2;
+        //	return resultNumber;
+        //      }
+        //private static int NanumTwoParam(int number1, int number2)
+        //      {
+        //	int resultNumber = number1 / number2;
+        //	return resultNumber;
+		//private static void Str(int n)
+  //      {
+		//	int n1 = 0;
+		//	for (;n>n1;n1++)
+  //          {
+		//		Console.WriteLine("문장 반복 횟수 : {0}", n1+1);
+  //          }
+			
+  //      }
+	}
+}
